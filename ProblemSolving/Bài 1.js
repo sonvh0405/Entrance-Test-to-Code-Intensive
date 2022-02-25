fe3e5f75d53734f93f217f8a7c5950906b6defd1
sonvh0405 <sonvh0405@gmail.com>
2022-02-25 23:32:57 +0700

    fix text

--- ProblemSolving/Bài 1.js
+++ ProblemSolving/Bài 1.js
@@ -5,7 +5,7 @@ function findOppositeNumber(n, inputNumber) {
         count = count + 1;
       }
       let sodoi = n / 2 + (count - 1);
-      console.log("Số đối diện với " + inputNumber + " là số: " + sodoi);
+      console.log("Số đối diện với số " + inputNumber + " là số: " + sodoi);
     } else {
       for (let i = n / 2; i <= inputNumber; i++) {
         count = count + 1;
